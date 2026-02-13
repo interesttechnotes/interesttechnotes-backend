c2c9c652b364e1642a773671d76c65244e2d2e48
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-3.local>
2026-02-14 02:18:52 +0530

    old code - from - https://blogtrial.vercel.app

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,13 +1,22 @@
 import nodemailer from "nodemailer";
 
 export const sendOtpEmail = async (email, otp) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS, // App password
+//     },
+//   });
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, // App password
+        user: process.env.EMAIL_USER,     // Your email
+        pass: process.env.EMAIL_PASS, // Your email password or app password
     },
-  });
+});
 console.log("--send mail to--",email);
 
   await transporter.sendMail({
