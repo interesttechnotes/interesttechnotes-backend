0771c3060e9e31dc4b7785d0c58a485be1d504db
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-3.local>
2026-02-14 02:31:47 +0530

    now use resend -test

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,33 +1,54 @@
 import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOtpEmail = async (email, otp) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS, // App password
-//     },
-//   });
-const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: process.env.EMAIL_USER,     // Your email
-        pass: process.env.EMAIL_PASS, // Your email password or app password
-    },
-});
-console.log("--send mail to--",email);
+    //   const transporter = nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //       user: process.env.EMAIL_USER,
+    //       pass: process.env.EMAIL_PASS, // App password
+    //     },
+    //   });
+    // const transporter = nodemailer.createTransport({
+    //     host: process.env.SMTP_HOST,
+    //     port: process.env.SMTP_PORT,
+    //     secure: false, // true for 465, false for other ports
+    //     auth: {
+    //         user: process.env.EMAIL_USER,     // Your email
+    //         pass: process.env.EMAIL_PASS, // Your email password or app password
+    //     },
+    // });
+    // console.log("--send mail to--", email);
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Your OTP Code",
-    html: `
-      <h2>Your OTP Code</h2>
-      <p>Your OTP is:</p>
-      <h1>${otp}</h1>
-      <p>This OTP will expire in 5 minutes.</p>
-    `,
-  });
+    // await transporter.sendMail({
+    //     from: process.env.EMAIL_USER,
+    //     to: email,
+    //     subject: "Your OTP Code",
+    //     html: `
+    //   <h2>Your OTP Code</h2>
+    //   <p>Your OTP is:</p>
+    //   <h1>${otp}</h1>
+    //   <p>This OTP will expire in 5 minutes.</p>
+    // `,
+    // });
+    try {
+        const response = await resend.emails.send({
+            from: "onboarding@resend.dev", // temporary test sender
+            to: email,
+            subject: "Your OTP Code",
+            html: `
+        <h2>Your OTP Code</h2>
+        <p>Your OTP is:</p>
+        <h1>${otp}</h1>
+        <p>This OTP will expire in 5 minutes.</p>
+      `,
+        });
+
+        console.log("Email sent:", response);
+    } catch (error) {
+        console.error("Resend Error:", error);
+        throw error;
+    }
 };
