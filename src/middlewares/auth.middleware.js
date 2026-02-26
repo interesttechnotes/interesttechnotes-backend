da82e1558f0f877074676f22bd78d50f9593107e
abkr020 <abkr.020@gmail.com>
2026-02-26 23:47:43 +0530

    token verify part log

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+  console.log("---verifyToken---");
   const authHeader = req.headers.authorization;
+  console.log("---authHeader---",authHeader);
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
   }
 
   const token = authHeader.split(" ")[1];
+  console.log("TOKEN:", token);
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
