f66a6e21662eca5d39f0951e0e26d45e9ec3f962
abkr020 <abkr.020@gmail.com>
2026-03-15 17:31:33 +0530

    added return

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,32 +1,33 @@
 import {
-//   sendContactEmailToAdmin,
-  sendContactThankYouEmail,
+    //   sendContactEmailToAdmin,
+    sendContactThankYouEmail,
 } from "../services/email.service.js";
 
 export const contactUs = async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
+    try {
+        console.log("contact us rout hit")
+        const { name, email, message } = req.body;
 
-    if (!name || !email || !message) {
-      return res.status(400).json({
-        message: "All fields are required",
-      });
+        if (!name || !email || !message) {
+            return res.status(400).json({
+                message: "All fields are required",
+            });
+        }
+
+        // send message to admin
+        // await sendContactEmailToAdmin(name, email, message);
+
+        // send thank you mail to user
+        await sendContactThankYouEmail(name, email, message);
+
+        return res.json({
+            success: true,
+            message: "Message sent successfully",
+        });
+    } catch (error) {
+        console.error("Contact Error:", error);
+        res.status(500).json({
+            message: "Failed to send message",
+        });
     }
-
-    // send message to admin
-    // await sendContactEmailToAdmin(name, email, message);
-
-    // send thank you mail to user
-    await sendContactThankYouEmail(name, email,message);
-
-    res.json({
-      success: true,
-      message: "Message sent successfully",
-    });
-  } catch (error) {
-    console.error("Contact Error:", error);
-    res.status(500).json({
-      message: "Failed to send message",
-    });
-  }
 };
